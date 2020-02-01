0a55175c0362275e809f2a77035eee937ad434a1
joerob99 <josephnrobinson1999@gmail.com>
2020-02-01 17:20:29 -0500

    Added HP bar script to be used in the future

---
+++ Robo Repair/Assets/Scripts/HPScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPScript : MonoBehaviour
+{
+    [SerializeField] Sprite[] hpStages;
+    [SerializeField] SpriteRenderer mySR;
+
+    // Testing health bar
+    /*void Update() {
+        if (Input.GetKey(KeyCode.K)) {
+            if (test == 0) { upOrDown = false; }
+            else if (test == 20) { upOrDown = true; }
+
+            if (!upOrDown) { test++; }
+            else { test--; }
+            mySR.sprite = hpStages[test]; }}*/
+
+    void UpdateHP(int hp)
+    {
+        mySR.sprite = hpStages[hp];
+    }
+}
