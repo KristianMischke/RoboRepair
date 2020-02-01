4d9d3e7289d7ca3c4be36ded353b9b177e76fbde
kristianmischke <kristianmischke@gmail.com>
2020-01-31 22:59:42 -0500

    Setting up AirConsole

---
+++ Robo Repair/Assets/_Scripts/GameLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameLogic : MonoBehaviour
+{
+    void Start()
+    {
+        AirConsole.instance.onReady += OnReady;
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+
+    void OnReady(string code)
+    {
+        
+    }
+
+    void OnMessage(int from, JToken data)
+    {
+        Debug.Log("message: " + data);
+        //AirConsole.instance.Message(from, "Full of pixels!");
+    }
+
+    void OnDestroy()
+    {
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+            AirConsole.instance.onReady -= OnReady;
+            //AirConsole.instance.onConnect -= OnConnect;
+        }
+    }
+}
