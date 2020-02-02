21d6dec99df64366c47d5b27482d5ebdb029940b
joerob99 <josephnrobinson1999@gmail.com>
2020-02-02 13:58:24 -0500

    Added UI Scene with editable text

---
+++ Robo Repair/Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] TMP_Text leftText, rightText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //leftText = GetComponentInChildren(TextMeshPro, false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLeftText(string text) { leftText.SetText(text); }
+    public void ChangeRightText(string text) { rightText.SetText(text); }
+}
