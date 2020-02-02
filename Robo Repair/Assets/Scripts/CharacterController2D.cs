99ec36cc86584150d5f1543aaa5aed7da897a95a
Craig <craigwstone@gmail.com>
2020-02-02 13:42:56 -0500

    Implemented player names

--- Robo Repair/Assets/Scripts/CharacterController2D.cs
+++ Robo Repair/Assets/Scripts/CharacterController2D.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json.Linq;
+using UnityEngine.UI;
+using TMPro;
+
 
 [RequireComponent(typeof(Collider2D))]
 public class CharacterController2D : MonoBehaviour
@@ -61,6 +64,9 @@ public class CharacterController2D : MonoBehaviour
 
     [SerializeField]
     HPScript myHPBar;
+
+    [SerializeField]
+    TMP_Text myText;
 
     // Visuals
     List<Sprite> bodySprites;
@@ -205,6 +211,10 @@ public class CharacterController2D : MonoBehaviour
         wireFrameTimer = WireFrameInterval;
 
         myHPBar.UpdateHP(hitpoints);
+
+        //myText.text = "Player " + playerID;
+
+        myText.SetText("Player " + playerID);
     }
 
     private void Update()
